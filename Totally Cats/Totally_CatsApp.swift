57840b76a010b33cc4009fdd1ec82fa213b6982f
jonsalkin <50082962+jonsalkin@users.noreply.github.com>
2023-06-22 23:03:36 -0400

    Initial Commit

---
+++ Totally Cats/Totally_CatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Totally_CatsApp.swift
+//  Totally Cats
+//
+//  Created by Jon Salkin on 6/22/23.
+//
+
+import SwiftUI
+
+@main
+struct Totally_CatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
